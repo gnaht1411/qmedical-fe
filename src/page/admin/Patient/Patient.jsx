546f35e5fb4a775d9389cce4c53b6ed2754b69a3
gnaht1411 <thanghongocql@gmail.com>
2022-03-25 10:46:29 +0700

    Update danh sach benh nhan (FE)

--- src/page/admin/Patient/Patient.jsx
+++ src/page/admin/Patient/Patient.jsx
@@ -1,47 +1,56 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { Button, Divider, message, Form, Select } from 'antd';
-import { EyeFilled, EyeInvisibleOutlined, SearchOutlined } from '@ant-design/icons';
-import { Table } from 'antd';
-import { Content } from 'antd/lib/layout/layout';
-import BreadCrumd from '../../../component/admin/breadcrumb/BreadCrumd';
+import React, { useEffect, useState } from "react";
+import { Button, Divider, message, Popconfirm, Space, Input } from "antd";
+import { EditOutlined, PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Table } from "antd";
+import { Content } from "antd/lib/layout/layout";
+import BreadCrumd from "../../../component/admin/breadcrumb/BreadCrumd";
+import CommonForm from "../../../component/admin/form/CommonForm";
+import axiosInstance from "../../../api/axiosInstance";
+
+const Search = Input.Search;
 
 const Patient = () => {
+  const columns = [
+    {
+      title: "Tên bệnh nhân",
+      dataIndex: "lastName",
+      sorter: (a, b) => a.lastName.localeCompare(b.lastName),
+    },
+    {
+      title: "Ngày sinh",
+      dataIndex: "dob",
+    },
+    {
+      title: "Điện thoại",
+      dataIndex: "phone",
+    },
+    {
+      title: "Giới tính",
+      dataIndex: "gender",
+      sorter: (a, b) => a.gender.localeCompare(b.gender),
+    },
+    {
+      title: "Địa chỉ",
+      dataIndex: "address",
+      sorter: (a, b) => a.address.localeCompare(b.address),
+    },
 
-    const columns = [
-        {
-            title: 'Tên bệnh nhân',
-            dataIndex: 'lastName',
-            sorter: (a, b) => a.lastName.localeCompare(b.lastName),
-        },
-        {
-            title: 'Ngày sinh',
-            dataIndex: 'dob',
-        },
-        {
-            title: 'Điện thoại',
-            dataIndex: 'phone',
-
-        },
-        {
-            title: 'Giới tính',
-            dataIndex: 'gender',
-            sorter: (a, b) => a.gender.localeCompare(b.gender),
-        },
-        {
-            title: 'Địa chỉ',
-            dataIndex: 'address',
-            sorter: (a, b) => a.address.localeCompare(b.address),
-        },
-
-        {
-            title: 'Action',
-            dataIndex: '',
-            key: 'x',
-            render: (item) => <>
-                <Button type='primary' icon={item.display === 1 ? <EyeFilled /> : <EyeInvisibleOutlined />}></Button>
-            </>,
-        },
-    ];
+    {
+      title: "Action",
+      dataIndex: "",
+      key: "x",
+      render: (item) => (
+        <>
+          <Button
+            type="primary"
+            onClick={() => handleFetchItem(item.id)}
+            shape="round"
+            icon={<EditOutlined />}
+          ></Button>
+        </>
+      ),
+    },
+  ];
 
   const [data, setData] = useState([]);
   const [item, setItem] = useState({});
@@ -58,127 +67,115 @@ const Patient = () => {
   };
 
   useEffect(() => {
-    console.log("GET PATIENT");
-
+    console.log("GET Patient");
     getPatient();
   }, []);
 
   const getPatient = async () => {
     setLoading(true);
     try {
-      const resPatient = await axiosInstance.getNoAuth(`patient`);
+      const resPatient = await axiosInstance.getNoAuth("patient/no-page");
       setData(resPatient.data);
-    } catch (e) {
-      createToast(toastTypes.ERROR, `Error !!!!`);
+    } catch (err) {
+      message.error(err ? err.response.data.message : err.message);
     }
     setLoading(false);
   };
 
   const handleFetchItem = async (id) => {
-    //setLoading(true);
+    setLoading(true);
     try {
-      const res = await axiosInstance.search(`/patient/search/${id}`);
+      const res = await axiosInstance.search(`/patient/${id}`);
       console.log("res fetchItem :", res);
       setItem(res.data);
-      console.log("item :", res.data);
+      console.log("item :", res.data.id);
       onOpenModal();
-      //setLoading(false);
+      setLoading(false);
     } catch (err) {
-      message.error(err.response.data.message);
+      message.error(err ? err.response.data.message : err.message);
       console.log(err);
     }
+  };
 
-    useEffect(() => {
-        console.log("GET PATIENT")
+  const handleDelete = async (id) => {
+    setLoading(true);
+    try {
+      const res = await axiosInstance.deletePatient(`/patient/${id}`);
+      getPatient();
+      message.success("Xóa thành công");
+      setLoading(false);
+    } catch (err) {
+      message.error(err ? err.response.data.message : err.message);
+      console.log(err);
+    }
+  };
 
+  const handleSubmit = async (item) => {
+    if (item?.id) {
+      try {
+        console.log("edit : ", item.id);
+        const res = await axiosInstance.putPatient(`/patient`, item);
         getPatient();
-    }, []);
+        message.success("Sửa thành công");
+        onCloseModal();
+      } catch (err) {
+        message.error(err ? err.response.data.message : err.message);
 
-    const getPatient = async () => {
-        setLoading(true);
-        try {
-            const resPatient = await axiosInstance.getNoAuth(`/patient`)
-            setData(resPatient.data);
-        } catch (e) {
-            createToast(toastTypes.ERROR, `Error !!!!`)
-            console.log(data)
-        }
-        setLoading(false);
+        console.log(err);
+      }
+    } else {
+      try {
+        const res = await axiosInstance.postPatient(`/patient`, item);
+        getPatient();
+        message.success("Thêm mới thành công !");
+        onCloseModal();
+      } catch (err) {
+        message.error(err ? err.response.data.message : err.message);
+        console.log(err);
+      }
     }
+  };
 
+  return (
+    <Content style={{ margin: "0 16px" }}>
+      <BreadCrumd title="Dịch vụ" subtitle="Danh sách" />
+      <div
+        className="site-layout-background"
+        style={{ padding: 24, minHeight: 360 }}
+      >
+        <Space
+          direction="horizontal"
+          style={{ width: "100%", justifyContent: "left" }}
+        >
+          {/* <Link to="add"> */}
+          <Button
+            type="primary"
+            htmlType="submit"
+            icon={<PlusOutlined />}
+            onClick={onOpenModal}
+          >
+            Thêm Bệnh nhân
+          </Button>
+          {/* </Link> */}
+        </Space>
 
+        <Search
+          placeholder="Search ..."
+          style={{ width: "30%", float: "right", marginTop: "-30px" }}
+        />
+        <Divider />
 
-    const handleFetchItem = async (id) => {
-        //setLoading(true);
-        try {
-            const res = await axiosInstance.search(`/patient/${id}`);
-            console.log('res fetchItem :', res);
-            setItem(res.data);
-            console.log('item :', res.data);
-            onOpenModal();
-            //setLoading(false);
-        } catch (err) {
-            message.error(err.response.data.message)
-            console.log(err);
-        }
-    };
-
-
-    const handleDelete = async (id) => {
-        setLoading(true);
-        try {
-            const res = await axiosInstance.deletePatient(`/patient/${id}`);
-            getPatient();
-            message.success(res.data.message)
-            setLoading(false);
-        } catch (err) {
-            message.error(err.response.data.message)
-            console.log(err);
-        }
-    }
-
-    const handleSubmit = async (item) => {
-        if (item?.id) {
-            try {
-                console.log('edit : ', item.id);
-                const res = await axiosInstance.postPatient(`/patient/${item.id}`, item);
-                console.log('res Edit :', res);
-                getPatient();
-                message.success(res.data.message)
-                onCloseModal();
-            } catch (err) {
-                message.error(err.response.data.message)
-                console.log(err);
-            }
-        } else {
-            try {
-                const res = await axiosInstance.postPatient(`/patient`, item);
-                console.log('res Add :', res);
-                getPatient();
-                message.success(res.data.message)
-                onCloseModal();
-            } catch (err) {
-                message.error(err.response.data.message)
-                console.log(err);
-            }
-        }
-    };
-
-    return (
-        <Content style={{ margin: '0 16px' }}>
-            <BreadCrumd title='Bệnh Nhân' subtitle='Danh sách' />
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                {<Space direction="horizontal" style={{ width: '100%', justifyContent: 'left' }}>
-                    <Button type="primary" htmlType="submit" icon={<PlusOutlined />} onClick={onOpenModal}>
-                        Thêm bệnh nhân
-                    </Button>
-                </Space>}
-                <Divider />
-                <Table loading={loading} columns={columns} dataSource={data} />
-                {<CommonForm fields={columns} handleSubmit={handleSubmit} item={item} openModal={openModal} onCloseModal={onCloseModal} />}
-            </div>
-        </Content>
-    );
-}
+        <Table loading={loading} columns={columns} dataSource={data} />
+        <CommonForm
+          fields={columns}
+          handleSubmit={handleSubmit}
+          item={item}
+          openModal={openModal}
+          onCloseModal={onCloseModal}
+        />
+      </div>
+    </Content>
+  );
+};
 
 export default Patient;
